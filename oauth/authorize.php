672577897c5f82a803d50b8c9a27c30317da252a
SKS Parnic <chris@straykitestudios.com>
2020-03-10 11:27:24 -0500

    Better oauth page layouts/scaling on mobile

--- oauth/authorize.php
+++ oauth/authorize.php
@@ -43,6 +43,7 @@ if (empty($_POST)) {
 		integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 		<link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
 
 	<body>
