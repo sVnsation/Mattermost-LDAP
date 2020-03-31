271d36966818f8231022483d2a478f441551d123
Mattermost <mattermost@mm.bibliothek.uni-halle.de>
2020-03-31 10:58:55 +0200

    1.fix in config_db.php 2.change to explicit versions centos + postgres 3.change path /var/www/html/oa to not confilct with mattermost url /oauth/ 4.change git repo

--- Docker/oauth/files/config_db.php
+++ Docker/oauth/files/config_db.php
@@ -6,7 +6,7 @@ $db_name	  = getenv('db_name') ?: "oauth_db";
 $db_type 	  = getenv('db_type') ?: "pgsql";
 $db_user 	  = getenv('db_user') ?: "oauth";
 $db_pass 	  = getenv('db_pass') ?: "oauth_secure-pass";
-$dsn	      = $type . ":dbname=" . $name . ";host=" . $host . ";port=" . $port;
+$dsn	      = $db_type . ":dbname=" . $db_name . ";host=" . $db_host . ";port=" . $db_port;
 
 /* Uncomment the line below to set date.timezone to avoid E.Notice raise by strtotime() (in Pdo.php)
  * If date.timezone is not defined in php.ini or with this function, Mattermost could return a bad token request error
